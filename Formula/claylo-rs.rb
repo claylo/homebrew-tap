41481c926552ef211ba7747f553b6694ee2fce17
Clay Loveless <clay@loveless.net>
2026-02-04 13:02:51 -0500

    chore: add claylo-rs initial release (#1)

---
+++ Formula/claylo-rs.rb
@@ -0,0 +1,33 @@
+# typed: true
+# frozen_string_literal: true
+
+class ClayloRs < Formula
+  desc "Production-ready Rust CLI scaffolding with updatable templates"
+  homepage "https://github.com/claylo/claylo-rs"
+  url "https://github.com/claylo/claylo-rs/archive/refs/tags/v1.0.0-alpha.1.tar.gz"
+  sha256 "PLACEHOLDER"
+  license "MIT"
+
+  depends_on "bash" => :build
+  depends_on "copier"
+
+  def install
+    bin.install "claylo-rs/bin/claylo-rs"
+  end
+
+  def caveats
+    <<~EOS
+      claylo-rs requires bash 4+ (provided by Homebrew's bash).
+      The script will use Homebrew's bash automatically.
+
+      Quick start:
+        claylo-rs new ./my-tool --owner myorg --copyright "Your Name"
+        claylo-rs new ./my-tool --preset full +otel
+        claylo-rs update ./my-tool +mcp
+    EOS
+  end
+
+  test do
+    assert_match "Usage: claylo-rs", shell_output("#{bin}/claylo-rs --help")
+  end
+end
