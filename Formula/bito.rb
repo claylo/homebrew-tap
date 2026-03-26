64b211b85a22b8a64aadcc14e78a830866b9634a
Clay Loveless <clay@loveless.net>
2026-03-25 20:00:24 -0400

    update bito sha256 values

--- Formula/bito.rb
+++ Formula/bito.rb
@@ -10,20 +10,20 @@ class Bito < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/claylo/bito/releases/download/v1.0.0/bito-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 ""
+      sha256 "706362e68cd2279bfd772576c1cd7e4745a8b2567f6107e6f893664d295978c9"
     else
       url "https://github.com/claylo/bito/releases/download/v1.0.0/bito-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 ""
+      sha256 "19490d97efcd09167a21820eabe89d71cbbddc61b76de2263fb95e55867261f6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/claylo/bito/releases/download/v1.0.0/bito-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 ""
+      sha256 "a267875f0ce4dfa4c358610831fedf811e60d9aac83fc63ce51e41827dc8805f"
     else
       url "https://github.com/claylo/bito/releases/download/v1.0.0/bito-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 ""
+      sha256 "8d22cb522185cd65841627e288181bd15de2613c7d7505742e50b61f33fff2f5"
     end
   end
 
