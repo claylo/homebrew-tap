6e3a0f1c59d3ae476bd72734d9608f423afb58de
Clay Loveless <clay@loveless.net>
2026-04-05 00:36:20 +0000

    scrat 0.1.0

---
+++ Formula/scrat.rb
@@ -0,0 +1,41 @@
+# typed: true
+# frozen_string_literal: true
+
+class Scrat < Formula
+  desc "Release management tooling focused on sanity retention"
+  homepage "https://github.com/claylo/scrat"
+  version "0.1.0"
+  license "Apache-2.0 OR MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/claylo/scrat/releases/download/v0.1.0/scrat-#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 ""
+    else
+      url "https://github.com/claylo/scrat/releases/download/v0.1.0/scrat-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 ""
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/claylo/scrat/releases/download/v0.1.0/scrat-#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 ""
+    else
+      url "https://github.com/claylo/scrat/releases/download/v0.1.0/scrat-#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 ""
+    end
+  end
+
+  def install
+    bin.install "bin/scrat"
+    man1.install Dir["share/man/man1/*.1"]
+    bash_completion.install "share/completions/scrat.bash" => "scrat"
+    zsh_completion.install "share/completions/_scrat"
+    fish_completion.install "share/completions/scrat.fish"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("\#{bin}/scrat --version-only")
+  end
+end
